a14b1f1b06398766d696b70144120d5e363d51e8
Kaioxen <hermawanwildan78@gmail.com>
2025-12-22 07:22:29 +0000

    i can use float :o

--- 00-getting-started/float.c
+++ 00-getting-started/float.c
@@ -3,6 +3,10 @@
 int main()
 {
 	float _x = 3.2;
-	printf("_x = %f ", _x); //f 
+	float _y = 4.2;
+	float _z = 5.2000000;
+	printf("_x = %f\n ", _x); //f
+	printf("_y = %.1f\n", _y); //1
+	printf("_z %.2f", _z); //2 
 	return 0; 	
 }

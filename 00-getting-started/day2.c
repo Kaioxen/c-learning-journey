51c59050befb487db1d5c3ea3e2f9bf9306326c2
Kaioxen <hermawanwildan78@gmail.com>
2025-12-23 06:34:29 +0000

    push

---
+++ 00-getting-started/day2.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int _age = 17;
+    int _birth_year = 2008;
+
+    printf("Hello, umur saya %d years old, dan ulang tahun saya, %d", _age, _birth_year);
+	return 0;
+}

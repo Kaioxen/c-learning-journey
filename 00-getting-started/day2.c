612a1c0ca428ac6e210a0d55d8a1b1ada65a0ea6
Kaioxen <hermawanwildan78@gmail.com>
2025-12-23 07:29:55 +0000

    push: masih ngaco

--- 00-getting-started/day2.c
+++ 00-getting-started/day2.c
@@ -3,10 +3,10 @@
 
 int main()
 {
-    int _umur = 17;
+  //  int _umur = 17;
     int _ulang_tahun = 2008;
     int _tanggal = 16;
     int _bulan = 02;
-    printf("Hello, umur saya %d years old, dan ulang tahun saya, %d %d %d", _umur, _tanggal, _bulan, _ulang_tahun);
+    printf("ulang tahun saya, %d %d %d",  _tanggal, _bulan, _ulang_tahun);
 	return 0;
 }

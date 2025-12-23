3336ecbb8c8e5bd7253c8a0c6ae33d8528141690
Kaioxen <hermawanwildan78@gmail.com>
2025-12-23 07:33:42 +0000

    push: PEA bet gua wkkw

--- 00-getting-started/day2.c
+++ 00-getting-started/day2.c
@@ -4,9 +4,9 @@
 int main()
 {
   //  int _umur = 17;
-    int _ulang_tahun = 2008;
+    int _tahun = 2008;
     int _tanggal = 16;
     int _bulan = 02;
-    printf("ulang tahun saya, %d %d %d",  _tanggal, _bulan, _ulang_tahun);
+    printf("tanggal lahir saya, %d %d %d",  _tanggal, _bulan, _tahun);
 	return 0;
 }

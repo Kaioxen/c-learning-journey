9b0fa95efc71ac12af13c135333398f1d96a72ca
Kaioxen <hermawanwildan78@gmail.com>
2025-12-25 12:35:20 +0000

    push

--- 01-control-flow/if-else.c
+++ 01-control-flow/if-else.c
@@ -1,18 +1,32 @@
 #include <stdio.h>
 
 int main()
-{  int _x;  
+{  int _x;
+   int _a = 10;  
    printf("hasil nilai : ");
    scanf("%d", &_x);
    if (_x == 17) {
-   	printf("nilai yang kamu masukan  17\n");
+      	printf("nilai yang kamu masukan  17\n");
    }
-    else if (_x == 16) {
+   else if (_x == 16) {
         printf("nilai yang kamu masukan 16\n");
     }
-    else  {
-    printf("nilai yang kamu masukan salah\n");   	
+   else if (17 >= 11) {
+      	printf("...");
+    } 
+   if ( _a == _x) {
+    	printf("haloo");
+    } 
+   else {
+    	printf("berantem yok,, anjayy");
+    }
+   if  (_x == 11)  {
+        printf("nilai yang kamu masukan salah\n");   	
+   } 
+   else {
+      	printf("masukan no ini %d", _a );
    }
+   
    
    return 0;
 }

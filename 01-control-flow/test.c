6ac539255feabe704ebed2e1e45b427e8ea75407
Kaioxen <hermawanwildan78@gmail.com>
2025-12-25 14:17:03 +0000

    push

--- 01-control-flow/test.c
+++ 01-control-flow/test.c
@@ -3,15 +3,15 @@
 int main() 
 {   
     int _nilai = 74;
-    printf("HAI"); 
-    printf("status nilai : ");
-    scanf("%d", &_nilai);
+        printf("HAI"); 
+        printf("status nilai : ");
+        scanf("%d", &_nilai);
     if (_nilai > 65 ) {
-      printf("lulus\n");    	
+        printf("lulus\n");    	
     } else {
-    	printf("gagal\n");
+     	printf("gagal\n");
     }
-    printf("END");
+        printf("END");
     
 	return 0;
 }
---
+++ Documentation/Cheatsheet.md
@@ -0,0 +1,13 @@
+# CHEATSHEET C 
+## Input/Output
+printf("text %d", var);    // print integer
+scanf("%d", &var);         // read integer
+
+## Loop Pattern
+for(init; condition; update) { }
+while(condition) { }
+do { } while(condition);
+
+## Common Patterns
+Swap: temp = a; a = b; b = temp;
+Min: min = (a < b) ? a : b;

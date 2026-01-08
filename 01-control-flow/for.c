eff8d12b858e6052dd6beaa37d3692fe06247321
Kaioxen <hermawanwildan78@gmail.com>
2026-01-08 13:21:49 +0000

    fix: string dan float

--- 01-control-flow/for.c
+++ 01-control-flow/for.c
@@ -7,7 +7,7 @@ int main()
  char _maaf[] = "maafin gua ";
  float _sum = 999;
 for (int i = 1; i <= 1000; i++) {
-    printf("%s %d %f.1\n", _maaf, i, _sum);  // Output maafin gua 1, maafin gua 2, dst...
+    printf("%s %d %.1f\n", _maaf, i, _sum);  // Output maafin gua 1, maafin gua 2, dst...
 }
 return 0;
 

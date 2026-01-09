4616ad47378ba587ebfaf2b270214fad17a92ddf
Kaioxen <hermawanwildan78@gmail.com>
2026-01-09 13:58:03 +0000

    feat(variables): contoh kapan pakai int vs float untuk bilangan

--- 00-getting-started/practice/h-why/why-newline.c
+++ 00-getting-started/practice/h-why/why-newline.c
@@ -8,7 +8,7 @@ int main()
     printf("Tampa newline");
     printf(" -> jadi atu baris");
     printf("\nDengan newline\n");
-    prinrf("-> jadi tepisah\n");
+    printf("-> jadi tepisah\n");
 
 
     return 0;
---
+++ 00-getting-started/practice/v-why/why-int.c
@@ -0,0 +1,22 @@
+
+/* Q: Kenapa pilih int bukan float? */
+
+#include <stdio.h>
+
+int main()
+{
+
+
+
+
+    int umur = 17;     //int untuk bilangan bulat
+    float tinggi = 160.5;     //float untuk desimal
+    printf("Umur: %d Tahun\n", umur);
+    printf("Tinggi: %.1f cm\n", tinggi);
+
+
+
+
+    return 0;
+}
+

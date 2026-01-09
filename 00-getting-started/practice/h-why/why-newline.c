ac42d82bdfbb7b2b607bf15ddb5fb1141535a48b
Kaioxen <hermawanwildan78@gmail.com>
2026-01-09 13:44:21 +0000

    feat(basics): demo efek karakter \n pada output

--- 00-getting-started/practice/h-why/why-newline.c
+++ 00-getting-started/practice/h-why/why-newline.c
@@ -1,11 +1,15 @@
 
+/* Q: Kenapa perlu \n ?? */
+
 #include <stdio.h>
 
 int main()
 {
+    printf("Tampa newline");
+    printf(" -> jadi atu baris");
+    printf("\nDengan newline\n");
+    prinrf("-> jadi tepisah\n");
 
 
-
-
- return 0;
+    return 0;
 }
